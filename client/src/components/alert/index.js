223b403e4dc8a76dc523685f5a07428888b5da68
TranAnhTu17 <20020495@vnu.edu.vn>
2022-10-07 21:06:15 +0700

    feat: add toast message

--- client/src/components/alert/index.js
+++ client/src/components/alert/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "./Loading";
+import ToastMessage from "./ToastMessage";
 
 const Alert = () => {
   const { alert } = useSelector((state) => state);
@@ -10,9 +11,17 @@ const Alert = () => {
     <div>
       {alert.loading && <Loading />}
 
-      {alert.error && <h1>{alert.error}</h1>}
+      {alert.error && (
+        <ToastMessage title="Error" message={alert.error} bgColor="danger" />
+      )}
 
-      {alert.success && <h1>{alert.success} </h1>}
+      {alert.success && (
+        <ToastMessage
+          title="Success"
+          message={alert.success}
+          bgColor="success"
+        />
+      )}
     </div>
   );
 };
