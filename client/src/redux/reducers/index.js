10ef4aa720c6796d96a175bf511274ca71956c7a
quysterben <sterben@LAPTOP-MSGPMRRR.localdomain>
2022-10-08 15:07:31 +0700

    feat: add create and detail flashcard page

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import alertReducer from "./alertReducer";
 import authReducer from "./authReducer";
+import flashcardReducer from "./flashcardReducer";
 import quizReducer from "./quizReducer";
 
 export default combineReducers({
-  auth: authReducer,
-  alert: alertReducer,
-  quiz: quizReducer,
+    auth: authReducer,
+    alert: alertReducer,
+    quiz: quizReducer,
+    flashcard: flashcardReducer,
 });
